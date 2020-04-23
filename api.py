aff5fad1224bcc5326c1cfca68c308a10ca2bae7
Hammedb197 <hammedb197@gmail.com>
2020-04-23 23:35:54 +0100

    ai

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 
+import detectron2
 from extracting import img_
-from extract_from_images import extract_from_images
+from extract_from_image import extract_from_images
 import flask
 from flask_cors import CORS
-from flask import request, jsonify
+from flask import request, jsonify, render_template, secure_filename
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
@@ -13,10 +14,10 @@ import numpy as np
 
 from PIL import Image
 import math
+import os
 
 
-
-def prepare_predictor(data):
+def prepare_predictor(file):
     # create config
     cfg = get_cfg()
     # below path applies to current installation location of Detectron2
@@ -28,7 +29,7 @@ def prepare_predictor(data):
     # boxes = outputs['instances'].pred_boxes.tensor.cpu().numpy()[0]
     classes = ['text', 'title', 'list', 'table', 'figure']
     default_predictor = detectron2.engine.defaults.DefaultPredictor(cfg)
-    img = detectron2.data.detection_utils.read_image(data, format="BGR")
+    img = detectron2.data.detection_utils.read_image(file, format="BGR")
     print("Predictor has been initialized.")
     predictions = default_predictor(img)
     instances = predictions["instances"].to('cpu')
@@ -37,47 +38,61 @@ def prepare_predictor(data):
 
 app = flask.Flask(__name__)
 CORS(app)
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+DEBUG = True
+
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = "K\x98\xa3\x89s\x146=\xe5\x97s\x17"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/api", methods=["POST"])
 def process_score_image_request():
     if request.methods == "POST":
-        data = request.files['file']
-        img, instances, classes = prepare_predictor(data)
-
-        pred_classes = instances.pred_classes
-        labels = [classes[i] for i in pred_classes]
-    # print(labels)
-        boxes = instances.pred_boxes
-        if isinstance(boxes, detectron2.structures.boxes.Boxes):
-            boxes = boxes.tensor.numpy()
-        else:
-            boxes = np.asarray(boxes)
-
-        for label, bbox in zip(labels, boxes):
-         
-            # getting prediction bboxes from model outputs
-        
-            x2 = math.ceil(bbox[0])
-            x1 = math.ceil(bbox[1])
-            y2 = math.ceil(bbox[2])
-            y1 = math.ceil(bbox[3])
-            crop_img = img[x1:y1,x2:y2]
-            print(len(crop_img))
-            if len(crop_img) <= 8:
-                continue
-            if label == "table":
-                table_ = img_(crop_img[ : , : , -1])
-                print("----------------")
-                print(label)
-                print("----------------")
-                print(table_.head(10))
-            elif label != "figure":
-                print("----------------")
-                print(label)
-                print("----------------")
-                print(extract_from_images(crop_img))
-
-        return 'done'
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # image = cv2.imread(os.path.dirname(os.path.realpath(__file__))+"/uploads/"+filename)
+            img, instances, classes = prepare_predictor("/uploads/" + filename)
+      
+            pred_classes = instances.pred_classes
+            labels = [classes[i] for i in pred_classes]
+            # print(labels)
+            boxes = instances.pred_boxes
+            if isinstance(boxes, detectron2.structures.boxes.Boxes):
+                boxes = boxes.tensor.numpy()
+            else:
+                boxes = np.asarray(boxes)
+       
+            for label, bbox in zip(labels, boxes):
+               
+                # getting prediction bboxes from model outputs
+            
+                x2 = math.ceil(bbox[0])
+                x1 = math.ceil(bbox[1])
+                y2 = math.ceil(bbox[2])
+                y1 = math.ceil(bbox[3])
+                crop_img = img[x1:y1,x2:y2]
+                print(len(crop_img))
+                if len(crop_img) <= 8:
+                    continue
+                if label == "table":
+                    table_ = img_(crop_img[ : , : , -1])
+                    print("----------------")
+                    print(label)
+                    print("----------------")
+                    print(table_.head(10))
+                elif label != "figure":
+                    print("----------------")
+                    print(label)
+                    print("----------------")
+                    print(extract_from_images(crop_img))
+       
+        return render_template('index.html')
 
 app.run(host="0.0.0.0", port=8899, debug=True)
